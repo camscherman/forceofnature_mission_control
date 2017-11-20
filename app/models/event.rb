a22019325acee08349972901b8aaa8859d31e3d8
Bang Tran <bangotran21@gmail.com>
2017-11-19 16:11:58 -0800

    added validations for event times

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,19 @@ class Event < ApplicationRecord
   has_many :attending, dependent: :destroy
   has_many :attendees, through: :attending, source: :user
 
+  validate :valid_start_date, on: :create
+  validate :valid_end_date, on: :create
+
+  def valid_start_date
+    if self.start_time < DateTime.now
+      errors.add(:start_time, "can't be in the past")
+    end
+  end
+
+  def valid_end_date
+    if self.end_time < start_time
+      errors.add(:end_time, "can't be before start_time")
+    end
+  end
+
 end
